804c185f719b41d44adf891c3c80e136a54489e8
Iamneo <techadmin@iamneo.ai>
2023-11-26 09:51:04 +0000

    Backup Commit

---
+++ angularapp/src/models/job-application.model.ts
@@ -0,0 +1,9 @@
+import { JobPosition } from "./job-position.model"
+
+export class JobApplication {
+    id:number
+    jobPositionId:number
+    jobPosition :JobPosition
+    applicantName:string
+    status: string
+}
---
+++ angularapp/src/models/job-position.model.ts
@@ -0,0 +1,4 @@
+export class JobPosition {
+    id:number
+    title:st
+}
